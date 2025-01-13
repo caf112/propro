ee438f4a7fe76a6bf4a0b70afeb41846ae14e2c5
caf112 <caf112.gt@gmail.com>
2025-01-14 05:18:16 +0900

    topへ

--- src/app/routes/not-found.tsx
+++ src/app/routes/not-found.tsx
@@ -1,8 +1,14 @@
+import { paths } from "@/config/paths"
+import { Link } from "react-router-dom"
+
 const NotFoundRoute = () => {
   return (
     <div>
         <h1>NotFoundPage</h1>
         <img src="../../character/usa_cry.png" alt="Not Found" />
+        <div>
+            <Link to={paths.Top.path} replace>Topへ</Link>
+        </div>
     </div>
   )
 }
