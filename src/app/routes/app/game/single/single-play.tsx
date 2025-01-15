d7b65852ad06e3ee9c54c3689d9afd5f33dc95ca
caf112 <caf112.gt@gmail.com>
2025-01-16 06:43:03 +0900

    propro-appをコピー

--- src/app/routes/app/game/single/single-play.tsx
+++ src/app/routes/app/game/single/single-play.tsx
@@ -1,20 +1,34 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { paths } from '@/config/paths';
+import * as Game from '@/features/game/index'
 import * as Button from '@/components/ui/button';
+import * as Loader from '@/components/ui/loader';
 
 const SinglePlayRoute = () => {
   const navigate = useNavigate()
-  const [timeLeft, setTimeLeft] = useState(60);
+  // const [timeLeft, setTimeLeft] = useState(60);
+
+  // useEffect(() => {
+  //   if (timeLeft > 0) {
+  //     const timerId = setInterval(() => {
+  //       setTimeLeft(timeLeft - 1);
+  //     }, 1000);
+  //     return () => clearInterval(timerId);
+  //   }
+  // }, [timeLeft]);
+
+  const [isLoading,setIsLoading] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
-    if (timeLeft > 0) {
-      const timerId = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 1000);
-      return () => clearInterval(timerId);
-    }
-  }, [timeLeft]);
+    setIsLoading(true);
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    },1000)
+
+    return () => clearTimeout(timeout)
+  },[location.pathname])
 
   const handleResultPage = () => {
     navigate(paths.game.single.result.getHref())
@@ -23,12 +37,14 @@ const SinglePlayRoute = () => {
   return (
     <div>
       <h1>Play</h1>
+      {isLoading && <Loader.Loader />}
+      {!isLoading && (
+        <>
+          <Game.CodeGame />
+        </>
+      )}
       <div>
-        <p>Game</p>
-        <p>Time Left: {timeLeft} seconds</p>
-      </div>
-      <div>
-          <Button.ActionButton onClick={handleResultPage} label="回答" iconClass='result-icon' />
+          {/* <Button.ActionButton onClick={handleResultPage} label="回答" iconClass='result-icon' /> */}
         </div>
     </div>
   )
