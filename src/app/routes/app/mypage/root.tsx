5df5fba1bca1d848a19886e6f571214b76710bce
caf112 <caf112.gt@gmail.com>
2025-01-15 23:53:47 +0900

    windowのheaderを作成

--- src/app/routes/app/mypage/root.tsx
+++ src/app/routes/app/mypage/root.tsx
@@ -2,15 +2,13 @@ import { DraggableWindow } from "@/components/ui/draggable-window"
 import { BackGroundLayout } from "@/components/layouts/background/background"
 import { Outlet } from "react-router-dom"
 
-
 const MyPageRoot = () => {
-  
   return (
-      <BackGroundLayout>
-        <DraggableWindow title="mypage" defaultX={150} defaultY={-700}>
-          <Outlet />
-        </DraggableWindow>
-      </BackGroundLayout>
+    <BackGroundLayout>
+      <DraggableWindow title="mypage" defaultX={150} defaultY={-700}>
+        <Outlet />
+      </DraggableWindow>
+    </BackGroundLayout>
   )
 }
 
--- src/components/ui/draggable-window/draggable-window.tsx
+++ src/components/ui/draggable-window/draggable-window.tsx
@@ -9,23 +9,42 @@ interface DesktopWindowProps {
     defaultX?: number;
     defaultY?: number;
   }
+  
+  const headerIcon = ((title: string) => {
+    if (title == "game") {
+      return imagePaths.favicon
+    } 
+    if (title == "mypage") {
+      return imagePaths.icon.mypage
+    }
+  })
 
 export const DraggableWindow: React.FC<DesktopWindowProps> = ({ title, children, defaultX = 150, defaultY = -700 }) => {
   const nodeRef = useRef<HTMLDivElement>(null);
-  return (
+
+return (
     <Draggable 
       nodeRef={nodeRef}
-      defaultPosition={{ x: defaultX, y: defaultY }}>
-        <div className="flame" ref={nodeRef}>
-    <div className="background"></div>
-    <img className="overlay-image" src={imagePaths.desktop.window} alt="Overlay" />
-    <div className="window">
-        <div className="window-header">
-            <h4>{title}</h4>
-        </div>
-        <div className="window-content">{children}</div>
-    </div>
-</div>
+      defaultPosition={{ x: defaultX, y: defaultY }}
+    >
+      <div className="flame" ref={nodeRef}>
+        <div className="background"></div>
+        <img className="overlay-image" src={imagePaths.desktop.window} alt="Overlay" />
+
+          <div className="window">
+            <div className="window-header">
+              <img className="page-icon" src={headerIcon(title)} alt={title} />
+              <h4>{title}</h4>
+              {/* <div className="window-controls">
+                <button className="minimize" title="Minimize"></button>
+                <button className="maximize" title="Maximize"></button>
+                <button className="close" title="Close"></button>
+              </div> */}
+            </div>
+            <div className="window-content">{children}</div>
+          </div>
+
+      </div>
     </Draggable>
   );
 };
